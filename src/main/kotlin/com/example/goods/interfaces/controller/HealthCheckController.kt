efbe437846a80586d627c0272e337272999a3623
taishi1116 <shinoda1116.miwa@gmail.com>
2022-05-14 14:30:35 +0900

    fix: ヘルスチェックエンドポイントの実装

--- src/main/kotlin/com/example/goods/interfaces/controller/HealthCheckController.kt
+++ src/main/kotlin/com/example/goods/interfaces/controller/HealthCheckController.kt
@@ -1,7 +1,6 @@
 package com.example.goods.interfaces.controller
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.Mapping
 import org.springframework.web.bind.annotation.RestController
 
 
