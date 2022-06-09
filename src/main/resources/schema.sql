4c507c15d0131f67228a6e026effa2634c3f8a75
taishi1116 <shinoda1116.miwa@gmail.com>
2022-06-10 07:26:49 +0900

    feat: trelloのdb構造定義

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,81 +1,79 @@
 -- もし既存のテーブルがあるなら削除して作り直す
 DROP TABLE IF EXISTS DEPT;
 
-
-CREATE TABLE USERS
+create table users
 (
-    id         CHAR(26)  NOT NULL,
-    name       VARCHAR   NOT NUll,
-    email      VARCHAR   NOT NULL,
-    password   VARCHAR   NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
+    id         CHAR(26)  not null,
+    name       CHAR(50)  not null,
+    email      CHAR(50)  not null,
+    password   CHAR(50)  not null,
+    created_at TIMESTAMP not null,
+    updated_at TIMESTAMP not null,
+    primary key (id)
 );
 
-CREATE TABLE TEAMS
+create table teams
 (
-    id      CHAR(26) NOT NULL,
-    user_id CHAR(26) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES USERS (id),
+    id      CHAR(26) not null,
+    user_id CHAR(26) not null,
+    primary key (id),
+    foreign key (user_id) references users (id)
 );
 
 
-CREATE TABLE TEAM_USERS
+create table team_users
 (
-    id      CHAR(26) NOT NULL,
-    team_id CHAR(26) NOT NULL,
-    user_id CHAR(26) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (team_id) REFERENCES TEAMS (id),
-    FOREIGN KEY (user_id) REFERENCES TEAMS (id)
-)
+    id      CHAR(26) not null,
+    team_id CHAR(26) not null,
+    user_id CHAR(26) not null,
+    primary key (id),
+    foreign key (team_id) references teams (id),
+    foreign key (user_id) references teams (id)
+);
 
-CREATE TABLE BOARDS
+create table boards
 (
-    id         CHAR(26)  NOT NULL,
-    team_id    CHAR(26)  NOT NULL,
-    title      CHAR(50)  NOT NULL,
-    image_url  TEXT      NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (team_id) REFERENCES TEAMS (id)
-)
+    id         CHAR(26)  not null,
+    team_id    CHAR(26)  not null,
+    title      CHAR(50)  not null,
+    image_url  text      not null,
+    created_at TIMESTAMP not null,
+    primary key (id),
+    foreign key (team_id) references teams (id)
+);
 
 
-CREATE TABLE CARD
+create table card
 (
-    id              CHAR(26) NOT NULL,
-    created_user_id CHAR(26) NOT NULL,
-    title           CHAR(50) NOT NULL,
-    description     TEXT     NOT NULL,
-    title           CHAR(50) NOT NULL,
-    manager_id      CHAR(26) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (created_user_id) REFERENCES USERS (id),
-    FOREIGN KEY (manager_id) REFERENCES USERS (id),
-)
+    id              CHAR(26) not null,
+    created_user_id CHAR(26) not null,
+    title           CHAR(50) not null,
+    description     text     not null,
+    manager_id      CHAR(26) not null,
+    primary key (id),
+    foreign key (created_user_id) references users (id),
+    foreign key (manager_id) references users (id)
+);
 
-CREATE TABLE CARD_LISTS
+create table card_lists
 (
-    id      CHAR(26) NOT NULL,
-    card_id CHAR(26) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (card_id) REFERENCES CARD (id)
-)
+    id      CHAR(26) not null,
+    card_id CHAR(26) not null,
+    primary key (id),
+    foreign key (card_id) references card (id)
+);
 
 
-CREATE TABLE COLUMNS
+create table columns
 (
-    id           CHAR(26) NOT NULL,
-    board_id     CHAR(26) NOT NULL,
-    card_list_id CHAR(26) NOT NULL,
-    index        INTEGER  NOT NULL,
-    name         CHAR(20) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (board_id) REFERENCES BOARDS (id),
-    FOREIGN KEY (card_list_id) REFERENCES BOARDS (id),
-)
+    id           CHAR(26) not null,
+    board_id     CHAR(26) not null,
+    card_list_id CHAR(26) not null,
+    position INTEGER not null,
+    name         CHAR(50) not null,
+    primary key (id),
+    foreign key (board_id) references boards (id),
+    foreign key (card_list_id) references boards (id)
+);
 
 
